35dc5d0ad129fcb52422eb71a8817f27786c5db2
sagorahmeed <sagorahmeed6060@gmail.com>
2021-06-13 15:40:31 +0600

    fixed isses

--- assets/styles.js
+++ assets/styles.js
@@ -119,3 +119,35 @@ tabTogglers.forEach(function (toggler) {
 
 document.getElementById("default-tab").click();
 //tab
+
+//modal
+const modal = document.querySelector('.main-modal');
+    const closeButton = document.querySelectorAll('.modal-close');
+
+    const modalClose = () => {
+        modal.classList.remove('fadeIn');
+        modal.classList.add('fadeOut');
+        setTimeout(() => {
+            modal.style.display = 'none';
+        }, 500);
+    }
+
+    const openModal = () => {
+        modal.classList.remove('fadeOut');
+        modal.classList.add('fadeIn');
+        modal.style.display = 'flex';
+    }
+
+    for (let i = 0; i < closeButton.length; i++) {
+
+        const elements = closeButton[i];
+
+        elements.onclick = (e) => modalClose();
+
+        modal.style.display = 'none';
+
+        window.onclick = function (event) {
+            if (event.target == modal) modalClose();
+        }
+    }
+    //modal
