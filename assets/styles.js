e9330bf033a88b25722e6dedf599d878bcba4938
sagorahmeed <sagorahmeed6060@gmail.com>
2021-06-17 09:32:36 +0600

    fixed issues

--- assets/styles.js
+++ assets/styles.js
@@ -145,8 +145,8 @@ function showSlides(n) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+        dots[i].className = dots[i].className.replace(" active_dot", "");
     }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    dots[slideIndex - 1].className += " active_dot";
 }
