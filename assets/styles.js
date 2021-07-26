a31e12c1e34431886f0f7b69a997fbd977f06db8
sagorahmeed <sagorahmeed6060@gmail.com>
2021-07-26 13:35:30 +0600

    fixed issues

--- assets/styles.js
+++ assets/styles.js
@@ -174,3 +174,167 @@ $(document).ready(function(){
 
 //custom number
 
+function initMap() {
+    var default_view = {
+      lat: 43.66106,
+      lng: -79.39488
+    };
+  
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 9,
+      center: default_view
+    });
+    
+    var university_of_waterloo = new google.maps.Marker({
+      position: {
+        lat: 43.4723,
+        lng: -80.5449
+      },
+      map: map,
+      title: 'University of Waterloo'
+    });
+    
+    var university_of_toronto = new google.maps.Marker({
+      position: {
+        lat: 43.66106,
+        lng: -79.39488
+      },
+      map: map,
+      title: 'University of Toronto'
+    });
+  
+    var laurier_university = new google.maps.Marker({
+      position: {
+        lat: 43.4724,
+        lng: -80.5263
+      },
+      map: map,
+      title: 'Laurier University'
+    });
+    
+    var university_of_guelph = new google.maps.Marker({
+      position: {
+        lat: 43.5309,
+        lng: -80.2285
+      },
+      map: map,
+      title: 'University of Guelph'
+    });
+    
+    var mcmaster_university = new google.maps.Marker({
+      position: {
+        lat: 43.2609,
+        lng: -79.9192
+      },
+      map: map,
+      title: 'McMaster University'
+    });
+    
+    var carleton_university = new google.maps.Marker({
+      position: {
+        lat: 45.3876,
+        lng: -75.6960
+      },
+      map: map,
+      title: 'Carleton University'
+    });
+    
+    var university_of_ottawa = new google.maps.Marker({
+      position: {
+        lat: 45.4231,
+        lng: -75.6831
+      },
+      map: map,
+      title: 'University of Ottawa'
+    });
+    
+    var york_university = new google.maps.Marker({
+      position: {
+        lat: 43.7735,
+        lng: -79.5019
+      },
+      map: map,
+      title: 'York University'
+    });
+    
+    var carleton_university = new google.maps.Marker({
+      position: {
+        lat: 45.5048,
+        lng: -73.5772
+      },
+      map: map,
+      title: 'McGill University'
+    });
+    
+    var university_of_houston = new google.maps.Marker({
+      position: {
+        lat: 29.7199,
+        lng: -95.3422
+      },
+      map: map,
+      title: 'University of Houston'
+    });
+    
+    var university_of_western_ontario = new google.maps.Marker({
+      position: {
+        lat: 43.0096,
+        lng: -81.2737
+      },
+      map: map,
+      title: 'University of Western Ontario'
+    });
+    
+    var ryerson_university = new google.maps.Marker({
+      position: {
+        lat: 43.6577,
+        lng: -79.3788
+      },
+      map: map,
+      title: 'Ryerson University'
+    });
+    
+    var queens_university = new google.maps.Marker({
+      position: {
+        lat: 44.2253,
+        lng: -76.4951
+      },
+      map: map,
+      title: 'Queen\'s University'
+    });
+    
+    var brock_university = new google.maps.Marker({
+      position: {
+        lat: 43.1176,
+        lng: -79.2477
+      },
+      map: map,
+      title: 'Brock University'
+    });
+    
+    var trent_university = new google.maps.Marker({
+      position: {
+        lat: 44.3569,
+        lng: -78.2902
+      },
+      map: map,
+      title: 'Trent University'
+    });
+    
+    var university_of_windsor = new google.maps.Marker({
+      position: {
+        lat: 42.3077,
+        lng: -83.0685
+      },
+      map: map,
+      title: 'University of Windsor'
+    });
+    
+    var tahoe = new google.maps.Marker({
+      position: {
+        lat: 39.0968,
+        lng: -120.0324
+      },
+      map: map,
+      title: 'Where my boys are at'
+    });
+  }
